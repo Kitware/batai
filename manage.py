5eae77d2eb9c61d8e2a0ed071c5279a9a135e2a9
naglepuff <mike.nagler@kitware.com>
2025-07-07 15:42:24 -0400

    Use uv as installer

--- manage.py
+++ manage.py
@@ -1,15 +1,10 @@
-#!/usr/bin/env python
-import os
+#!/usr/bin/env -S uv run
 import sys
 
 from django.core.management import execute_from_command_line
 
 
 def main() -> None:
-    # Production usage runs manage.py for tasks like collectstatic,
-    # so DJANGO_CONFIGURATION should always be explicitly set in production
-    os.environ.setdefault('DJANGO_CONFIGURATION', 'bats_ai.settings.development')
-
     execute_from_command_line(sys.argv)
 
 
