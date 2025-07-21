2e3c3245bbe5658967dd281d24d5a6527aa5ef39
Bryon Lewis <61746913+BryonLewis@users.noreply.github.com>
2025-07-21 08:16:19 -0400

    Refactor Spectrogram Viewers (#212)
    * refactor viewers, remove warnings, update tsconfig
    
    * refactor getImageDimensions

--- client/src/components/ThumbnailViewer.vue
+++ client/src/components/ThumbnailViewer.vue
@@ -5,42 +5,20 @@ import { SpectroInfo, useGeoJS } from './geoJS/geoJSUtils';
 import { OtherUserAnnotations, SpectrogramAnnotation } from "../api/api";
 import LayerManager from "./geoJS/LayerManager.vue";
 import geo, { GeoEvent } from "geojs";
+import { getImageDimensions } from "@use/useUtils";
+
 
 export default defineComponent({
   name: "ThumbnailViewer",
-  components: {
-    LayerManager
-  },
+  components: { LayerManager },
   props: {
-    images: {
-      type: Array as PropType<HTMLImageElement[]>,
-      default: () => [],
-    },
-    spectroInfo: {
-      type: Object as PropType<SpectroInfo | undefined>,
-      default: () => undefined,
-    },
-    annotations: {
-      type: Array as PropType<SpectrogramAnnotation[]>,
-      default: () => [],
-    },
-    otherUserAnnotations: {
-      type: Object as PropType<OtherUserAnnotations>,
-      default: () => ({}),
-    },
-    selectedId: {
-        type: Number as PropType<number | null>,
-        default: null,
-    },
-    recordingId: {
-      type: String as PropType<string | null>,
-      required: true,
-    },
-    parentGeoViewerRef: {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      type: Object as PropType<any>,
-      required: true,
-    }
+    images: { type: Array as PropType<HTMLImageElement[]>, default: () => [] },
+    spectroInfo: { type: Object as PropType<SpectroInfo | undefined>, default: () => undefined },
+    annotations: { type: Array as PropType<SpectrogramAnnotation[]>, default: () => [] },
+    otherUserAnnotations: { type: Object as PropType<OtherUserAnnotations>, default: () => ({}) },
+    selectedId: { type: Number as PropType<number | null>, default: null },
+    recordingId: { type: String as PropType<string | null>, required: true },
+    parentGeoViewerRef: { type: Object as PropType<any>, required: true }
   },
   emits: ['selected'],
   setup(props) {
@@ -49,144 +27,105 @@ export default defineComponent({
     const initialized = ref(false);
     const clientHeight = ref(0);
     const yScale = ref(1);
-    const polyLayerCreated= ref(false);
-    let downState: {
-      state: any,
-      mouse: any,
-      center: any,
-      rotate: any,
-      zoom:any,
-      distanceToOutline: any,
-    };
+    const polyLayerCreated = ref(false);
+    let downState: any;
+
+    function updateViewerAndImages() {
+      const { width, height } = getImageDimensions(props.images);
+      if (containerRef.value) {
+        clientHeight.value = containerRef.value.clientHeight;
+      }
+
+      if (containerRef.value && !geoJS.getGeoViewer().value) {
+        geoJS.initializeViewer(containerRef.value, width, height, true);
+      }
+      geoJS.resetMapDimensions(width, height);
+      geoJS.getGeoViewer().value.bounds({ left: 0, top: 0, bottom: height, right: width });
+
+      // Calculate yScale
+      const camera = geoJS.getGeoViewer().value.camera();
+      const coords = camera.worldToDisplay({ x: 0, y: 0 });
+      const end = camera.worldToDisplay({ x: 0, y: height });
+      const diff = coords.y - end.y;
+      yScale.value = diff ? (clientHeight.value * 0.5) / diff : 1;
+
+      if (props.images.length) {
+        geoJS.drawImages(props.images, width, height * yScale.value);
+      }
+      initialized.value = true;
+      nextTick(() => createPolyLayer());
+    }
 
     const createPolyLayer = () => {
+      if (polyLayerCreated.value) return;
       const geoViewer = geoJS.getGeoViewer();
-      const featureLayer = geoViewer.value.createLayer('feature', {feattures: ['polygon']});
+      const featureLayer = geoViewer.value.createLayer('feature', { features: ['polygon'] });
       const outlineFeature = featureLayer.createFeature('polygon', {});
       const outlineStyle = {
-          stroke: true,
-          strokeColor: 'yellow',
-          strokeWidth: 1,
-          fill: false,
+        stroke: true,
+        strokeColor: 'yellow',
+        strokeWidth: 1,
+        fill: false,
       };
       featureLayer.geoOff();
 
       featureLayer.geoOn(geo.event.mouseclick, (evt: GeoEvent) => {
-            props.parentGeoViewerRef.value.center(evt.geo);
+        props.parentGeoViewerRef.value.center(evt.geo);
       });
       featureLayer.geoOn(geo.event.actiondown, (evt: GeoEvent) => {
-            downState = {
-                state: evt.state,
-                mouse: evt.mouse,
-                center:  props.parentGeoViewerRef.value.center(),
-                zoom:  props.parentGeoViewerRef.value.zoom(),
-                rotate:  props.parentGeoViewerRef.value.rotation(),
-                distanceToOutline: geo.util.distanceToPolygon2d(evt.mouse.geo, outlineFeature.data()[0]) / props.parentGeoViewerRef.value.unitsPerPixel(props.parentGeoViewerRef.value.zoom())
-            };
-        });
-        featureLayer.geoOn(geo.event.actionmove, (evt: GeoEvent) => {
-            switch (evt.state.action) {
-                case 'overview_pan': {
-                    if (!downState ){ //|| downState.distanceToOutline < -this._panOutlineDistance) {
-                        return;
-                    }
-                    const delta = {
-                        x: evt.mouse.geo.x - downState.mouse.geo.x,
-                        y: evt.mouse.geo.y - downState.mouse.geo.y
-                    };
-                    const center = props.parentGeoViewerRef.value.center();
-                    delta.x -= center.x - downState.center.x;
-                    delta.y -= center.y - downState.center.y;
-                    if (delta.x || delta.y) {
-                      props.parentGeoViewerRef.value.center({
-                            x: center.x + delta.x,
-                            y: center.y + delta.y
-                        });
-                    }
-                }
-                    break;
-            }
-        });
-        const onParentPan = () => {
-          const parent = props.parentGeoViewerRef.value;
+        downState = {
+          state: evt.state,
+          mouse: evt.mouse,
+          center: props.parentGeoViewerRef.value.center(),
+          zoom: props.parentGeoViewerRef.value.zoom(),
+          rotate: props.parentGeoViewerRef.value.rotation(),
+          distanceToOutline: geo.util.distanceToPolygon2d(evt.mouse.geo, outlineFeature.data()[0]) /
+            props.parentGeoViewerRef.value.unitsPerPixel(props.parentGeoViewerRef.value.zoom())
+        };
+      });
+      featureLayer.geoOn(geo.event.actionmove, (evt: GeoEvent) => {
+        if (evt.state.action === 'overview_pan' && downState) {
+          const delta = {
+            x: evt.mouse.geo.x - downState.mouse.geo.x,
+            y: evt.mouse.geo.y - downState.mouse.geo.y
+          };
+          const center = props.parentGeoViewerRef.value.center();
+          delta.x -= center.x - downState.center.x;
+          delta.y -= center.y - downState.center.y;
+          if (delta.x || delta.y) {
+            props.parentGeoViewerRef.value.center({
+              x: center.x + delta.x,
+              y: center.y + delta.y
+            });
+          }
+        }
+      });
+
+      const onParentPan = () => {
+        const parent = props.parentGeoViewerRef.value;
         if (parent.rotation() !== props.parentGeoViewerRef.value.rotation()) {
-            props.parentGeoViewerRef.value.rotation(parent.rotation());
-            props.parentGeoViewerRef.value.zoom(props.parentGeoViewerRef.value.zoom());
+          props.parentGeoViewerRef.value.rotation(parent.rotation());
+          props.parentGeoViewerRef.value.zoom(props.parentGeoViewerRef.value.zoom());
         }
         const size = parent.size();
         const { top } = parent.bounds();
         outlineFeature.style(outlineStyle);
         const polygon = [[
-            parent.displayToGcs({x: 0, y:  clientHeight.value * 0.5 + top * yScale.value}),
-            parent.displayToGcs({x: size.width, y: clientHeight.value * 0.5 +top * yScale.value}),
-            parent.displayToGcs({x: size.width, y: clientHeight.value * 0.5 +(size.height * yScale.value) + (top * yScale.value)}),
-            parent.displayToGcs({x: 0, y: clientHeight.value * 0.5 + (size.height * yScale.value) + (top * yScale.value)})
+          parent.displayToGcs({ x: 0, y: clientHeight.value * 0.5 + top * yScale.value }),
+          parent.displayToGcs({ x: size.width, y: clientHeight.value * 0.5 + top * yScale.value }),
+          parent.displayToGcs({ x: size.width, y: clientHeight.value * 0.5 + (size.height * yScale.value) + (top * yScale.value) }),
+          parent.displayToGcs({ x: 0, y: clientHeight.value * 0.5 + (size.height * yScale.value) + (top * yScale.value) })
         ]];
         outlineFeature.data(polygon).draw();
-        };
-        onParentPan();
-        // Bind parent pan to the outline feature
-        props.parentGeoViewerRef.value.geoOff(geo.event.pan, onParentPan);
-        props.parentGeoViewerRef.value.geoOn(geo.event.pan, onParentPan);
-        polyLayerCreated.value = true;
+      };
+
+      onParentPan();
+      props.parentGeoViewerRef.value.geoOff(geo.event.pan, onParentPan);
+      props.parentGeoViewerRef.value.geoOn(geo.event.pan, onParentPan);
+      polyLayerCreated.value = true;
     };
 
-    watch(() => props.spectroInfo, () => {
-      let naturalWidth = 0;
-      let naturalHeight = 0;
-      props.images.forEach((image) => {
-        naturalWidth += image.naturalWidth;
-        naturalHeight = image.naturalHeight;
-      });
-      if (containerRef.value) {
-        clientHeight.value = containerRef.value.clientHeight;
-      }
-      geoJS.resetMapDimensions(naturalWidth, naturalHeight);
-      geoJS.getGeoViewer().value.bounds({
-      left: 0,
-      top: 0,
-      bottom: naturalHeight,
-      right: naturalWidth,
-    });
-
-      const coords = geoJS.getGeoViewer().value.camera().worldToDisplay({x: 0, y:0});
-        const end = geoJS.getGeoViewer().value.camera().worldToDisplay({x: 0, y:naturalHeight});
-        const diff = coords.y - end.y;
-        // How much space to we have to multiply the size of the image
-        yScale.value = (clientHeight.value *0.5) / diff;
-      if (props.images.length) {
-        geoJS.drawImages(props.images, naturalWidth, naturalHeight * yScale.value);
-      }
-      initialized.value = true;
-      nextTick(() => createPolyLayer());
-    });
-
-    watch(containerRef, () => {
-      let naturalWidth = 0;
-      let naturalHeight = 0;
-      props.images.forEach((image) => {
-        naturalWidth += image.naturalWidth;
-        naturalHeight = image.naturalHeight;
-      });
-      if (containerRef.value) {
-        clientHeight.value = containerRef.value.clientHeight;
-      }
-      if (containerRef.value && ! geoJS.getGeoViewer().value) {
-        geoJS.initializeViewer(containerRef.value, naturalWidth, naturalHeight, true);
-      }
-      const coords = geoJS.getGeoViewer().value.camera().worldToDisplay({x: 0, y:0});
-        const end = geoJS.getGeoViewer().value.camera().worldToDisplay({x: 0, y:naturalHeight});
-        const diff = coords.y - end.y;
-        // How much space to we have to multiply the size of the image
-        yScale.value = (clientHeight.value *0.5) / diff;
-      if (props.images.length) {
-        geoJS.drawImages(props.images, naturalWidth, naturalHeight * yScale.value);
-      }
-      initialized.value = true;
-        nextTick(() => createPolyLayer());
-
-    });
-
+    watch([() => props.spectroInfo, containerRef], updateViewerAndImages);
 
     return {
       containerRef,
