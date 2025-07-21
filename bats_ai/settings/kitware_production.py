7e22f9023fc4db8a9926cb17d223b7a1c1eb9e68
Bryon Lewis <bryon@batdetectai.kitware.com>
2025-07-21 18:02:20 +0000

    initial config changes for deployment

--- bats_ai/settings/kitware_production.py
+++ bats_ai/settings/kitware_production.py
@@ -2,10 +2,15 @@ from .production import *
 
 # Import these afterwards, to override
 from resonant_settings.testing.minio_storage import *  # isort: skip
+from environ import Env
+
+env = Env()
+SECURE_SSL_REDIRECT = False  # disable because we are using a reverse proxy (traefik that does it)
 
 STORAGES['default'] = {
     'BACKEND': 'minio_storage.storage.MinioMediaStorage',
 }
-
+BASE_HOST = env.str('DJANGO_ALLOWED_HOSTS', default='')
 # TODO: why does this need to be set?
-# CSRF_TRUSTED_ORIGINS = [f'https:/{BASE_HOST}', f'https://{BASE_HOST}']
+# This is so the client and communicate with django properly through the reverse proxy
+CSRF_TRUSTED_ORIGINS = [f'https://{BASE_HOST}']
--- bats_ai/settings/production.py
+++ bats_ai/settings/production.py
@@ -1,7 +1,8 @@
 from .base import *
 
 # Import these afterwards, to override
-from resonant_settings.production.email import *  # isort: skip
+# Removed email until we have SMTP server support
+# from resonant_settings.production.email import *  # isort: skip
 from resonant_settings.production.https import *  # isort: skip
 
 WSGI_APPLICATION = 'bats_ai.wsgi.application'
