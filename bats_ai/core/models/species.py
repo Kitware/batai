1f8ea65dc382ad03b4b7bc555ac2230db544e729
Bryon Lewis <Bryon.Lewis@kitware.com>
2025-08-04 09:32:49 -0400

    add category to species, update fixture, migration loads fixture

--- bats_ai/core/models/species.py
+++ bats_ai/core/models/species.py
@@ -2,9 +2,22 @@ from django.db import models
 
 
 class Species(models.Model):
+    CATEGORY_CHOICES = [
+        ('couplet', 'Couplet'),
+        ('individual', 'Individual Species'),
+        ('frequency', 'Frequency'),
+        ('noid', 'NoID'),
+    ]
+
     species_code = models.CharField(max_length=255, blank=True, null=True)
     family = models.CharField(max_length=255, blank=True, null=True)
     genus = models.CharField(max_length=255, blank=True, null=True)
     species = models.CharField(max_length=255, blank=True, null=True)
     common_name = models.CharField(max_length=255, blank=True, null=True)
     species_code_6 = models.CharField(max_length=255, blank=True, null=True)
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        default='individual',
+        help_text='Category label: couplet, individual species, frequency, or NoID',
+    )
