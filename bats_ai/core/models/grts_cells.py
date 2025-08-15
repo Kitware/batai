f9a0d19402e494e261f7f8c0978558c1b2d3b0b1
Bryon Lewis <Bryon.Lewis@kitware.com>
2025-08-15 09:38:12 -0400

    remove unnecessary fields from GRTS

--- bats_ai/core/models/grts_cells.py
+++ bats_ai/core/models/grts_cells.py
@@ -20,27 +20,8 @@ class GRTSCells(models.Model):
     grts_cell_id = models.IntegerField()
     sample_frame_id = models.IntegerField(blank=True, null=True)
     grts_geom = models.GeometryField(blank=True, null=True)
-    water_p = models.FloatField(blank=True, null=True)
-    outside_p = models.FloatField(blank=True, null=True)
-    location_1_type = models.CharField(max_length=255, blank=True, null=True)
-    location_1_name = models.CharField(max_length=255, blank=True, null=True)
-    location_1_p = models.FloatField(blank=True, null=True)
-    location_2_type = models.CharField(max_length=255, blank=True, null=True)
-    location_2_name = models.CharField(max_length=255, blank=True, null=True)
-    location_2_p = models.FloatField(blank=True, null=True)
-    sub_location_1_type = models.CharField(max_length=255, blank=True, null=True)
-    sub_location_1_name = models.CharField(max_length=255, blank=True, null=True)
-    sub_location_1_p = models.FloatField(blank=True, null=True)
-    sub_location_2_type = models.CharField(max_length=255, blank=True, null=True)
-    sub_location_2_name = models.CharField(max_length=255, blank=True, null=True)
-    sub_location_2_p = models.FloatField(blank=True, null=True)
-    own_1_name = models.CharField(max_length=255, blank=True, null=True)
-    own_1_p = models.FloatField(blank=True, null=True)
     # continue defining all fields similarly
-    priority_frame = models.BooleanField(blank=True, null=True)
-    priority_state = models.BooleanField(blank=True, null=True)
     geom_4326 = models.GeometryField()
-    clipped = models.BooleanField(blank=True, null=True)
 
     @property
     def sampleFrameMapping(self):
