84c64e84d87fb9e3f11cb7625fd2b9b06152c282
Bryon Lewis <61746913+BryonLewis@users.noreply.github.com>
2025-08-19 07:48:50 -0400

    Load GRTSCell Ids from Shapefiles (#225)
    * add geopandas to dependencies, add loadGRTS management command
    
    * remove unnecessary fields from GRTS
    
    * remove older importGRTSCells management command and documentation
    
    * add Mexico and Puerto Rico to shapefile list
    
    * Update DEPLOYMENT.md
    
    Co-authored-by: Michael Nagler <mike.nagler@kitware.com>
    
    * Update DEPLOYMENT.md
    
    Co-authored-by: Michael Nagler <mike.nagler@kitware.com>
    
    ---------
    
    Co-authored-by: Michael Nagler <mike.nagler@kitware.com>

--- bats_ai/core/admin/grts_cells.py
+++ bats_ai/core/admin/grts_cells.py
@@ -9,8 +9,5 @@ class GRTSCellsAdmin(admin.ModelAdmin):
         'id',
         'grts_cell_id',
         'sample_frame_id',
-        'water_p',
-        'outside_p',
     )  # Add other fields you want to display in the list
     search_fields = ('id', 'grts_cell_id', 'sample_frame_id')  # Add fields for searching
-    list_filter = ('location_1_type', 'location_2_type')  # Add fields for filtering
