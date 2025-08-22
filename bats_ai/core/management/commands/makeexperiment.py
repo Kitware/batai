d4116685dfa2c8a559d6d9cc09b3a0fa4700b0f3
naglepuff <mike.nagler@kitware.com>
2025-08-22 13:08:58 -0400

    Clean up mlflow manage commands

--- bats_ai/core/management/commands/makeexperiment.py
+++ bats_ai/core/management/commands/makeexperiment.py
@@ -1,15 +1,8 @@
 from django.conf import settings
-from django.contrib.auth.models import User
 import djclick as click
 from mlflow import MlflowClient
 
 
-@click.option(
-    '--username',
-    type=click.STRING,
-    required=True,
-    help='superuser username for mlflow experiment creation',
-)
 @click.option('--name', type=click.STRING, required=True, help='a name for the mlflow experiment')
 @click.option(
     '--description',
@@ -18,22 +11,13 @@ from mlflow import MlflowClient
     help='a description for the mlflow experiment',
 )
 @click.command()
-def command(username, name, description: str | None = None):
-    user = None
-    if username:
-        user = User.objects.get(username=username)
-    else:
-        first_user = User.objects.first()
-        if first_user:
-            user = user
+def command(name, description: str | None = None):
+    mlflow_client = MlflowClient(tracking_uri=settings.MLFLOW_ENDPOINT)
 
-    if user:
-        mlflow_client = MlflowClient(tracking_uri=settings.MLFLOW_ENDPOINT)
+    experiment_tags = {
+        'project-name': 'batsai',
+    }
+    if description:
+        experiment_tags['mlflow.note.content'] = description
 
-        experiment_tags = {
-            'project-name': 'batsai',
-        }
-        if description:
-            experiment_tags['mlflow.note.content'] = description
-
-        mlflow_client.create_experiment(name=name, tags=experiment_tags)
+    mlflow_client.create_experiment(name=name, tags=experiment_tags)
--- bats_ai/core/management/commands/registeronnxmodel.py
+++ bats_ai/core/management/commands/registeronnxmodel.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 from django.conf import settings
 import djclick as click
@@ -12,18 +13,19 @@ def command():
     relative = ('..',) * 5
     asset_path = os.path.abspath(os.path.join(__file__, *relative, 'assets'))
     onnx_filename = os.path.join(asset_path, 'model.mobilenet.onnx')
+    assert Path(onnx_filename).exists()
     onnx_model = onnx.load(onnx_filename)
 
     mlflow.set_tracking_uri(settings.MLFLOW_ENDPOINT)
-    mlflow.set_experiment('default')
+    mlflow.set_experiment('Default')
     with mlflow.start_run() as run:
         run_id = run.info.run_id
         click.echo(f'Run ID: {run_id}')
         mlflow_onnx.log_model(
             onnx_model=onnx_model,
-            artifact_path='onnx_model',
-            # save_as_external_data=True,
+            name='onnx_model',
+            registered_model_name="onnx-prototype"
         )
-        model_uri = f'runs:/{run_id}/onnx_model'
-        result = mlflow.register_model(model_uri=model_uri, name='prototype')
-        click.echo(f'Registered {model_uri} version {result.version}')
+        # model_uri = 'models:/onnx_model'
+        # result = mlflow.register_model(model_uri=model_uri, name='prototype')
+        # click.echo(f'Registered {model_uri} version {result.version}')
