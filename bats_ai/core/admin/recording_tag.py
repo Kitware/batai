156e19dfef91c90c21b79488436e01307cd60c4d
naglepuff <mike.nagler@kitware.com>
2025-11-17 16:55:22 -0500

    Add recording count to recording tag admin

--- bats_ai/core/admin/recording_tag.py
+++ bats_ai/core/admin/recording_tag.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Count
 
 from bats_ai.core.models import RecordingTag
 
@@ -9,5 +10,16 @@ class RecordingTagAdmin(admin.ModelAdmin):
         'id',
         'user',
         'text',
+        'recording_count',
     )
     search_fields = ('id', 'user', 'text')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.annotate(_recording_count=Count('recording'))
+
+    def recording_count(self, obj):
+        return obj._recording_count
+
+    recording_count.short_description = 'Number of recordings'
+    recording_count.admin_order_field = '_recording_count'
