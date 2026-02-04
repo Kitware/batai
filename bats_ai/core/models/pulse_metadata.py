10a85b8aec2872f6f9eb7c3062f93db7a5a30da2
Bryon Lewis <Bryon.Lewis@kitware.com>
2026-02-04 12:01:02 -0500

    make contours optional for pulseMetadata

--- bats_ai/core/models/pulse_metadata.py
+++ bats_ai/core/models/pulse_metadata.py
@@ -7,5 +7,5 @@ class PulseMetadata(models.Model):
     recording = models.ForeignKey(Recording, on_delete=models.CASCADE)
     index = models.IntegerField(null=False, blank=False)
     bounding_box = models.PolygonField(null=False, blank=False)
-    contours = models.JSONField()
+    contours = models.JSONField(null=True, blank=True)
     # TODO: Add in metadata from batbot
