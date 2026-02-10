0749c5f7f54bb243eb5c469ae11506e84d5c8402
naglepuff <mike.nagler@kitware.com>
2026-02-10 15:48:52 -0500

    Remove inference code
    This currently isn't supported in batbot

--- bats_ai/core/tasks/nabat/tasks.py
+++ bats_ai/core/tasks/nabat/tasks.py
@@ -4,13 +4,12 @@ import tempfile
 
 import requests
 
-from bats_ai.core.models import Configuration, ProcessingTask, Species
-from bats_ai.core.models.nabat import NABatRecording, NABatRecordingAnnotation
+from bats_ai.core.models import ProcessingTask
+from bats_ai.core.models.nabat import NABatRecording
 from bats_ai.core.utils.batbot_metadata import generate_spectrogram_assets
 from bats_ai.utils.spectrogram_utils import (
     generate_nabat_compressed_spectrogram,
     generate_nabat_spectrogram,
-    predict_from_compressed,
 )
 
 logging.basicConfig(level=logging.INFO)
@@ -54,51 +53,7 @@ def generate_spectrograms(
 
         compressed = results['compressed']
 
-        compressed_obj = generate_nabat_compressed_spectrogram(
-            nabat_recording, spectrogram, compressed
-        )
-
-        try:
-            config = Configuration.objects.first()
-            # TODO: Disabled until prediction is in batbot
-            # https://github.com/Kitware/batbot/issues/29
-            if config and config.run_inference_on_upload and False:
-                self.update_state(
-                    state='Progress',
-                    meta={'description': 'Running Prediction on Spectrogram'},
-                )
-
-                predict_results = predict_from_compressed(compressed_obj)
-                label = predict_results['label']
-                score = predict_results['score']
-                confs = predict_results['confs']
-                confidences = [
-                    {'label': key, 'value': float(value)} for key, value in confs.items()
-                ]
-                sorted_confidences = sorted(confidences, key=lambda x: x['value'], reverse=True)
-                output = {
-                    'label': label,
-                    'score': float(score),
-                    'confidences': sorted_confidences,
-                }
-                species = Species.objects.filter(species_code=label)
-
-                nabat_recording_annotation = NABatRecordingAnnotation.objects.create(
-                    nabat_recording=compressed_obj.nabat_recording,
-                    comments='Compressed Spectrogram Generation Prediction',
-                    model='model.mobilenet.onnx',
-                    confidence=output['score'],
-                    additional_data=output,
-                )
-                nabat_recording_annotation.species.set(species)
-                nabat_recording_annotation.save()
-
-        except Exception as e:
-            logger.error(f'Error Performing Prediction: {e}')
-            processing_task.status = ProcessingTask.Status.ERROR
-            processing_task.error = f'Error performing prediction: {e}'
-            processing_task.save()
-            raise
+        generate_nabat_compressed_spectrogram(nabat_recording, spectrogram, compressed)
 
         processing_task.status = ProcessingTask.Status.COMPLETE
         processing_task.save()
