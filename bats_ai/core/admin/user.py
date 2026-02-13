30c6aafdc139357e5394b32f948f655c47f41e69
naglepuff <mike.nagler@kitware.com>
2026-02-13 14:18:18 -0500

    Add verified filter to user admin

--- bats_ai/core/admin/user.py
+++ bats_ai/core/admin/user.py
@@ -11,11 +11,31 @@ class UserProfileInline(admin.StackedInline):
     extra = 0
 
 
+class UserIsVerifiedFilter(admin.SimpleListFilter):
+    title = 'verified'
+
+    parameter_name = 'verified'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('verified', 'verified'),
+            ('not_verified', 'not verified'),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == 'verified':
+            return queryset.filter(profile__verified=True)
+        if self.value() == 'not_verified':
+            return queryset.filter(profile__verified=False)
+        return queryset
+
+
 class UserAdmin(BaseUserAdmin):
     inlines = [UserProfileInline]
     list_select_related = ['profile']
 
     list_display = list(BaseUserAdmin.list_display) + ['is_verified']
+    list_filter = list(BaseUserAdmin.list_filter) + [UserIsVerifiedFilter]
 
     def is_verified(self, obj):
         return hasattr(obj, 'profile') and obj.profile.verified
