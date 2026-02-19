713667577185443c1014c6e6bac786c0e1005e45
naglepuff <mike.nagler@kitware.com>
2026-02-19 15:54:20 -0500

    Send email to admin on new user signup

--- bats_ai/core/models/user_profile.py
+++ bats_ai/core/models/user_profile.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.template.loader import render_to_string
+from django.urls import reverse
 
 
 class UserProfile(models.Model):
@@ -13,3 +16,19 @@ class UserProfile(models.Model):
 def _create_new_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def _notify_admins_new_user(sender, instance, created, **kwargs):
+    admins = User.objects.filter(is_superuser=True).all()
+    recipient_list = [admin.email for admin in admins]
+
+    if recipient_list:
+        new_user_url = reverse('admin:auth_user_change', args=[instance.pk])
+        email_content = render_to_string('core/new_user_signup.txt', {'new_user_url': new_user_url})
+        send_mail(
+            'New user signup',
+            email_content,
+            None,
+            recipient_list,
+        )
--- bats_ai/core/tests/test_user_profile.py
+++ bats_ai/core/tests/test_user_profile.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User
+from django.urls import reverse
 import pytest
 
 from bats_ai.core.models import UserProfile
+
+from .factories import SuperuserFactory
 
 
 @pytest.mark.django_db
@@ -11,3 +14,15 @@ def test_profile_creation():
     user = User.objects.create()
     profile = UserProfile.objects.get(user=user)
     assert not profile.verified
+
+
+@pytest.mark.django_db
+def test_new_user_signup_email_sent(mailoutbox):
+    superuser = SuperuserFactory()
+    user = User.objects.create(
+        username='foo',
+        email='foo@bar.com',
+    )
+    m = mailoutbox[0]
+    assert superuser.email in m.to
+    assert reverse('admin:auth_user_change', args=[user.pk]) in m.body
