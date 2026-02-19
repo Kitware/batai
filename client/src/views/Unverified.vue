3e7677e09ff1bb3d6436f696ce91009e98821d1a
naglepuff <mike.nagler@kitware.com>
2026-02-19 14:11:33 -0500

    Create page to send unverified users

---
+++ client/src/views/Unverified.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h2>Unverified</h2>
+  <p>Your account has not been verified.</p>
+  <p>Please contact an administrator to get your account verified and begin using this application.</p>
+</template>
