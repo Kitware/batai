a6e7586a2d76dc39f78ad53cfe64cbebc5b57529
Brian Helba <brian.helba@kitware.com>
2026-02-23 22:46:20 -0500

    Fix RUF005: use iterable unpacking instead of list concatenation

--- bats_ai/core/admin/user.py
+++ bats_ai/core/admin/user.py
@@ -22,8 +22,8 @@ class UserAdmin(BaseUserAdmin):
     list_select_related = ["profile"]
 
     # See https://code.djangoproject.com/ticket/36926#ticket
-    list_display = list(BaseUserAdmin.list_display) + ["is_verified"]
-    list_filter = list(BaseUserAdmin.list_filter) + ["profile__verified"]
+    list_display = [*BaseUserAdmin.list_display, "is_verified"]
+    list_filter = [*BaseUserAdmin.list_filter, "profile__verified"]
 
     @admin.display(
         boolean=True,
