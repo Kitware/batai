c72a3102a44ddb0bbf2075af5cf8439792122f86
Bryon Lewis <Bryon.Lewis@kitware.com>
2026-04-01 09:07:22 -0400

    modify default single point size

--- client/src/components/RecordingLocationsMap.vue
+++ client/src/components/RecordingLocationsMap.vue
@@ -192,7 +192,7 @@ export default defineComponent({
             filter: ['!', ['has', 'point_count']],
             paint: {
               'circle-color': '#11b4da',
-              'circle-radius': 5,
+              'circle-radius': 10,
               'circle-stroke-width': 1,
               'circle-stroke-color': '#fff',
             },
