6db3150f5c404cb6b3b77069f7baf21c68659a7f
Bryon Lewis <Bryon.Lewis@kitware.com>
2026-04-08 09:24:04 -0400

    swap to tuples to lists for admin

--- bats_ai/core/admin/species_range.py
+++ bats_ai/core/admin/species_range.py
@@ -8,16 +8,16 @@ from bats_ai.core.models import SpeciesRange
 
 @admin.register(SpeciesRange)
 class SpeciesRangeAdmin(GISModelAdmin):
-    list_display = (
+    list_display = [
         "id",
         "species",
         "source_feature_id",
-    )
-    list_select_related = ("species",)
-    search_fields = (
+    ]
+    list_select_related = ["species"]
+    search_fields = [
         "species__species_code",
         "species__common_name",
         "source_feature_id",
-    )
-    autocomplete_fields = ("species",)
-    ordering = ("species__species_code",)
+    ]
+    autocomplete_fields = ["species"]
+    ordering = ["species__species_code"]
